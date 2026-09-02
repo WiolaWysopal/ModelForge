dbf28f41b00da7d9fd4056d4d669f309b1d1b745
Wioletta Wysopal <wioletta.wysopal@gmail.com>
2026-09-02 11:32:14 +0200

    feat: add dataset database model

--- apps/backend/alembic/env.py
+++ apps/backend/alembic/env.py
@@ -8,6 +8,8 @@ from alembic import context
 from app.core.config import settings
 from app.core.database import Base
 
+from app.models.dataset import Dataset
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
